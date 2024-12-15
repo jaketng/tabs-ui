734253d29dc59704e9097bcf6f1ba5044af15171
Jake Bowden <jakebowden@Jakes-MacBook-Pro-2.local>
2024-12-15 16:40:32 -0500

    Configures tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div>
+        <p className="text-blue-500">
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
